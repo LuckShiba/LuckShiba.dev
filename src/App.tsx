12ae6532c85d287f5ebb8628f1f4848c73cf575b
LuckShiba <LuckShiba@protonmail.com>
2023-01-26 00:55:07 -0300

    remove: everything; changing to @vitejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
-import { BrowserRouter } from "react-router-dom";
-import Routes from "./Routes";
-import "./App.css";
-
-export default function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
-    </div>
-  );
+function App() {
+  return <div>I've become so numb, I can't feel you there.</div>;
 }
+
+export default App;
